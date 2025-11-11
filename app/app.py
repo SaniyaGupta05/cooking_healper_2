8eee94f68cbd12f13753e32cd375ea1d3cf2bbad
SaniyaGupta05 <saniyagupta708@gmail.com>
2025-11-11 16:34:35 +0530

    port change

--- app/app.py
+++ app/app.py
@@ -735,4 +735,4 @@ def internal_error(error):
     return jsonify({'error': 'Internal server error'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
